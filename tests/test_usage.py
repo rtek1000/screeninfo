6ff1fcabb315f1af8dd76ae1d42e46089e5887d3
rtek1000 <rh.silicon@gmail.com>
2025-02-21 14:42:33 -0300

    Create test_usage.py

---
+++ tests/test_usage.py
@@ -0,0 +1,10 @@
+    dict1 = get_screen()
+    print('width:', dict1.get('width'))
+
+    def get_screen(is_primary=True):
+        for m1 in screeninfo.get_monitors():
+            d1 = m1.__dict__
+            print(d1)
+            if d1.get('is_primary') == True:
+                print("Screen 0: ", int(d1.get('width')), "x", int(d1.get('height')))
+                return d1
