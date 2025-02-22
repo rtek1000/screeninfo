63c17f06768eed65833092bc7bc827f4ab92a74a
rtek1000 <rh.silicon@gmail.com>
2025-02-22 09:09:56 -0300

    Update test_usage.py

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -5,7 +5,7 @@ print('width:', dict1.get('width'))
 
 # screeninfo.get_monitors() sample:
 # [Monitor(x=0, y=0, width=1920, height=1080, width_mm=309, height_mm=174, name='\\\\.\\DISPLAY1', is_primary=True), Monitor(x=1920, y=0, width=1366, height=768, width_mm=256, height_mm=144, name='\\\\.\\DISPLAY2', is_primary=False)]    
-def get_screen(self, is_primary):
+def get_screen(is_primary=True):
     for m1 in screeninfo.get_monitors():
         dict1 = m1.__dict__
         print(dict1)
